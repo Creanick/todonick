66e7ba2d6a39c255413810a2627121363181134c
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-07 20:09:11 +0530

    timestamp converter improved

--- lib/helpers/timestamp_convert.dart
+++ lib/helpers/timestamp_convert.dart
@@ -1,7 +1,11 @@
-String toTimeStamp(DateTime dateTime) {
-  return dateTime.toIso8601String();
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+DateTime toTimeStamp(DateTime dateTime) {
+  if (dateTime == null) return null;
+  return dateTime;
 }
 
-DateTime fromTimeStamp(String dateString) {
-  return DateTime.parse(dateString);
+DateTime fromTimeStamp(Timestamp timestamp) {
+  if (timestamp == null) return null;
+  return DateTime.fromMicrosecondsSinceEpoch(timestamp.microsecondsSinceEpoch);
 }
