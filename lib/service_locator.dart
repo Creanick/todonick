8404ca50a86d0d9a20acdd548fc2a9f38148ad7b
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-11 11:17:31 +0530

    add database service in servicelocator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:todonick/services/auth_service.dart';
+import 'package:todonick/services/database_service.dart';
 
 final GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerSingleton(AuthService());
+  locator.registerSingleton(DatabaseService());
 }
