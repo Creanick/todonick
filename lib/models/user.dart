ad4c4700bea8920f52edfcbdb34c724d3b1261e5
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-12 10:16:00 +0530

    and profile url property in user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,18 +4,27 @@ class User {
   final String id;
   String _name;
   final String email;
+  String _profileUrl;
   String get name => _name;
-  User({@required this.id, @required name, @required this.email})
-      : _name = name;
+  String get profileUrl => _profileUrl;
+  User(
+      {@required this.id,
+      @required name,
+      @required this.email,
+      String profileUrl})
+      : _name = name,
+        _profileUrl = profileUrl;
 
   User.fromMap(this.id, Map<String, dynamic> userMap)
       : _name = userMap['name'],
-        email = userMap['email'];
+        email = userMap['email'],
+        _profileUrl = userMap['profileUrl'];
   Map<String, dynamic> toMap() {
-    return {"name": _name, "email": email};
+    return {"name": _name, "email": email, "profileUrl": _profileUrl};
   }
 
-  void udpate({String name}) {
+  void udpate({String name, String profileUrl}) {
     _name = name ?? _name;
+    _profileUrl = profileUrl ?? _profileUrl;
   }
 }
