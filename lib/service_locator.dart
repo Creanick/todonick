63e29f0353834f8fe75a42b3bcd3c82cee6e3aba
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-12 10:15:43 +0530

    add storage service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,10 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:todonick/services/auth_service.dart';
 import 'package:todonick/services/database_service.dart';
+import 'package:todonick/services/storage_service.dart';
 
 final GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerSingleton(AuthService());
   locator.registerSingleton(DatabaseService());
+  locator.registerSingleton(StorageService());
 }
