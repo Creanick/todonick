98aea61c7fa11654a9117763863ae318bca0a579
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-13 12:34:46 +0530

    todo list service created

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:todonick/helpers/failure.dart';
+import 'package:todonick/helpers/view_response.dart';
+import 'package:todonick/models/todo_list.dart';
 import 'package:todonick/models/user.dart';
 
 class DatabaseService {
   Firestore _store = Firestore.instance;
   CollectionReference _userCollection;
+  final String todoListCollectionName = "todoLists";
   DatabaseService([String userCollectionPath = "users"]) {
     _userCollection = _store.collection(userCollectionPath);
   }
@@ -51,6 +54,36 @@ class DatabaseService {
     } catch (error) {
       print(error);
       throw Failure("updating user failed");
+    }
+  }
+
+  Future<List<TodoList>> getTodoLists(String userId) async {
+    if (userId == null) throw Failure("user id is not available");
+    try {
+      final QuerySnapshot querySnapshot = await _userCollection
+          .document(userId)
+          .collection(todoListCollectionName)
+          .getDocuments();
+      if (querySnapshot == null) return [];
+      final lists = querySnapshot.documents.map((docSnapshot) {
+        return TodoList.fromMap(docSnapshot.documentID, docSnapshot.data);
+      }).toList();
+      return lists;
+    } catch (error) {
+      throw Failure("getting todo lists failed");
+    }
+  }
+
+  Future<TodoList> createTodoList(String userId, String todoListName) async {
+    if (userId == null) throw Failure("user id is not available");
+    try {
+      DocumentReference docRef = await _userCollection
+          .document(userId)
+          .collection(todoListCollectionName)
+          .add({"name": todoListName});
+      return TodoList(id: docRef.documentID, name: todoListName);
+    } catch (error) {
+      throw Failure("todo list creation failed");
     }
   }
 }
