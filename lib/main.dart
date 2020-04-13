0fbff9169ef13177e06de547eb939dd3fda78d8a
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-13 12:35:13 +0530

    add todo list provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 import 'package:todonick/auth_router.dart';
 import 'package:todonick/providers/auth_user_provider.dart';
+import 'package:todonick/providers/todo_list_provider.dart';
 import 'package:todonick/providers/todo_user_provider.dart';
+import 'package:todonick/screens/todo_list_edit_screen.dart';
 import 'package:todonick/screens/user_edit_screen.dart';
 import 'package:todonick/service_locator.dart';
 
@@ -26,11 +28,20 @@ class MyApp extends StatelessWidget {
             update: (_, authUserProvider, todoUserProvider) =>
                 todoUserProvider..fetchUser(authUserProvider?.authUser?.uid),
           ),
+          ChangeNotifierProxyProvider<AuthUserProvider, TodoListProvider>(
+              lazy: false,
+              create: (_) => TodoListProvider(),
+              update: (_, authUserProvider, todoListProvider) =>
+                  todoListProvider
+                    ..fetchTodoLists(authUserProvider?.authUser?.uid)),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           home: AuthRouter(),
-          routes: {UserEditScreen.routeName: (_) => UserEditScreen()},
+          routes: {
+            UserEditScreen.routeName: (_) => UserEditScreen(),
+            TodoListEditScreen.routeName: (_) => TodoListEditScreen(),
+          },
         ));
   }
 }
