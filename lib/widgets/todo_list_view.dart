10dc5ee6bb8a9b6aad95535bda5f936d302e7107
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-14 17:06:28 +0530

    create proper todo list view for completed and non completed todos

--- lib/widgets/todo_list_view.dart
+++ lib/widgets/todo_list_view.dart
@@ -2,22 +2,56 @@ import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 import 'package:todonick/models/todo.dart';
 import 'package:todonick/providers/todo_provider.dart';
-import 'package:todonick/providers/view_state_provider.dart';
 import 'package:todonick/widgets/todo_tile.dart';
 
-class TodoListView extends StatelessWidget {
+class TodoListView extends StatefulWidget {
+  @override
+  _TodoListViewState createState() => _TodoListViewState();
+}
+
+class _TodoListViewState extends State<TodoListView> {
+  bool isExpanded = false;
   @override
   Widget build(BuildContext context) {
     final TodoProvider todoProvider = Provider.of<TodoProvider>(context);
     final List<Todo> todos = todoProvider.todos;
-    return todoProvider.state == ViewState.loading
-        ? Center(
-            child: Text("Todos loading..."),
-          )
-        : todos.isEmpty
-            ? Center(child: Text("No Todos"))
-            : ListView.builder(
-                itemBuilder: (ctx, index) => TodoTile(todos[index], index),
-                itemCount: todos.length);
+    final List<TodoTile> nonCompletedTodoTile = [];
+    final List<TodoTile> completedTodoTile = [];
+    for (int i = 0; i < todos.length; i++) {
+      final todo = todos[i];
+      if (todo.completed) {
+        completedTodoTile.add(TodoTile(todo, i));
+      } else {
+        nonCompletedTodoTile.add(TodoTile(todo, i));
+      }
+    }
+    return todos == null || todos.isEmpty
+        ? Center(child: Text("No Todos"))
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              final int nonLength = nonCompletedTodoTile.length;
+              if (index < nonLength) {
+                return nonCompletedTodoTile[index];
+              }
+              if (index == nonLength) {
+                return ListTile(
+                    trailing: IconButton(
+                      icon: Icon(
+                          isExpanded ? Icons.expand_less : Icons.expand_more),
+                      onPressed: () {
+                        setState(() {
+                          isExpanded = !isExpanded;
+                        });
+                      },
+                    ),
+                    title: Text("Completed (${completedTodoTile.length})"));
+              }
+              final completedIndex = index - nonLength - 1;
+              return completedTodoTile[completedIndex];
+            },
+            itemCount: nonCompletedTodoTile.length +
+                (completedTodoTile.isEmpty
+                    ? 0
+                    : ((isExpanded ? completedTodoTile.length : 0) + 1)));
   }
 }
