d5ed738cba676074e49bc6713bba89034544060f
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-04-20 09:31:51 +0530

    add google sign in and sign out in auth service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:todonick/helpers/failure.dart';
 
 class AuthService {
+  GoogleSignIn _googleSignIn = GoogleSignIn();
   FirebaseAuth _auth = FirebaseAuth.instance;
 
   //signup
@@ -37,11 +39,30 @@ class AuthService {
   //sign out
   Future<void> signOut() async {
     try {
+      final bool isGoogleSignedIn = await _googleSignIn.isSignedIn();
+      if (isGoogleSignedIn) {
+        await _googleSignIn.signOut();
+      }
       await _auth.signOut();
     } on PlatformException catch (p) {
       throw Failure(p.message);
     } catch (error) {
       throw Failure("sign out failed");
+    }
+  }
+
+  Future<FirebaseUser> signInWithGoogle() async {
+    try {
+      GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      AuthCredential authCredential = GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      final AuthResult authResult =
+          await _auth.signInWithCredential(authCredential);
+      return authResult.user;
+    } catch (error) {
+      print(error);
+      throw Failure("Google sigin failed");
     }
   }
 
